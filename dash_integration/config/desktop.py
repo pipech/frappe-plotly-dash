4096cfded3a3de4c2b5a741fe050560b62ce1426
Poranut Chollavorn <pipech@gmail.com>
2019-11-02 05:58:50 +0000

    feat(desk): add dash page to desktop

--- dash_integration/config/desktop.py
+++ dash_integration/config/desktop.py
@@ -4,11 +4,22 @@ from frappe import _
 
 def get_data():
 	return [
+		# {
+		# 	'module_name': 'Dash Integration',
+		# 	'color': 'grey',
+		# 	'icon': 'octicon octicon-graph',
+		# 	'type': 'module',
+		# 	'label': _('Dash Integration')
+		# },
 		{
-			"module_name": "Dash Integration",
-			"color": "grey",
-			"icon": "octicon octicon-graph",
-			"type": "module",
-			"label": _("Dash Integration")
-		}
+			'module_name': 'dash',
+			'category': 'Places',
+			'label': _('Dash'),
+			'icon': 'octicon octicon-graph',
+			'type': 'link',
+			'link': '#dash',
+			'color': '#FF4136',
+			'standard': 1,
+			'idx': 11
+		},
 	]
