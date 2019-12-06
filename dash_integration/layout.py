1f6f96b281f1de7ae21014220022701e8abb2665
Poranut Chollavorn <pipech@gmail.com>
2019-12-06 10:03:04 +0000

    perf(css): move dashboard css to dash_dashboard app

--- dash_integration/layout.py
+++ dash_integration/layout.py
@@ -2,6 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from frappe import render_template
+from jinja2.exceptions import TemplateNotFound
 
 
 def config_layout(dash_app):
@@ -11,16 +12,27 @@ def config_layout(dash_app):
         html.Div(id='page-content'),
     ])
 
+    # get dashboard css from template
+    context = {'dashboard_css': ''}
+    try:
+        context['dashboard_css'] = render_template(
+            'templates/dashboard_config.css',
+            context={}
+        )
+    except TemplateNotFound:
+        pass
+
     # Override the underlying HTML template
     html_layout = dash_render_template(
         template_name='templates/dashboard.html',
+        context=context,
     )
     dash_app.index_string = html_layout
 
 
-def dash_render_template(template_name):
+def dash_render_template(template_name, context={}):
     # default render template by frappe
-    template = render_template(template_name, context={})
+    template = render_template(template_name, context=context)
 
     # replacing custom context
     template = template.replace('[%', '{%')
